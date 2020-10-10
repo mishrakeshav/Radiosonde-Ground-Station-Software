065eff24e22e0763a5e0ab4e0348a6a605a256b1
Saloni Patadia <saloni.patadia@somaiya.edu>
2020-10-10 20:12:10 +0530

    added

--- src/app/utils/Canvas.py
+++ src/app/utils/Canvas.py
@@ -6,11 +6,22 @@ from matplotlib.figure import Figure
 matplotlib.use('Qt5Agg')
 
 
+# class MplCanvas(FigureCanvas):
+#     def __init__(self, parent=None, width=5, height=4, dpi=100):
+#         fig = Figure(figsize=(width, height), dpi=dpi)
+#         self.axes = fig.add_subplot(111)
+
+#         FigureCanvas.__init__(self, fig)
+#         self.setParent(parent)
+#         FigureCanvas.updateGeometry(self)
+
+
+
+#NICHE KA RAKHO UPAR KA NAHI
+#HAMARE GRAPHS NICHE WALE KE KAARAN CHAL RAHE H
+
 class MplCanvas(FigureCanvas):
     def __init__(self, parent=None, width=5, height=4, dpi=100):
-        fig = Figure(figsize=(width, height), dpi=dpi)
-        self.axes = fig.add_subplot(111)
-
-        FigureCanvas.__init__(self, fig)
-        self.setParent(parent)
-        FigureCanvas.updateGeometry(self)
+        self.fig = Figure(figsize=(width, height), dpi=dpi)
+        self.axes = self.fig.add_subplot(111)
+        super(MplCanvas, self).__init__(self.fig)
